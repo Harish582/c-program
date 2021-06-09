99e7cd247c8305b2e97525f7de66e9206762c6b4
Harish582 <77111298+Harish582@users.noreply.github.com>
2021-06-09 00:50:33 -0700

    Add files via upload

--- count sort.c
+++ count sort.c
@@ -1,55 +1,52 @@
 #include<stdio.h>
 void main()
 {
-	int a[20],b[20],count[20]={0},i,j,k,n,temp;
-	printf("enter the size of array:");
-	scanf("%d",&n);
-	printf("enter the elements:");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-		
-	}
-	printf("the array is:");
-	for(i=0;i<n;i++)
-	{
-		printf("%d\n",a[i]);
-		
-	}
-	for(i=0;i<n;i++)
-	{
-		for(j=i+1;j<n;j++)
-		{
-			if(a[i]>a[j])
-			{
-				temp=a[i];
-				a[i]=a[j];
-				temp=a[j];
-				k=a[j];
-			}
-			break;
-		}
-	}
-	for(i=0;i<n;i++)
-	{
-		++count[a[i]];
-	}
-	for(i=1;i<=k;i++)
-	{
-		count[i]=count[i]+count[i-1];
-	}
-	for(i=n-1;i>=0;i--)
-	{
-		b[--count[a[i]]]=a[i];
-	}
-	for(i=0;i<n;i++)
-	{
-		a[i]=b[i];
-	}
-	for(i=0;i<n;i++)
-	{
-	   	printf("%d",a[i]);
-		
-	}
-	
+    int a[50],count[50]={0},n,i,max,temp,j,b[50];
+    printf("Enter the number of elements : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\nThe elements are : \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+                max=a[j];
+            }
+            break;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        ++count[a[i]];
+    }
+    for(i=1;i<=max;i++)
+    {
+        count[i]=count[i]+count[i-1];
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        b[--count[a[i]]]=a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        a[i]=b[i];
+    }
+    printf("\n The sorted array is : ");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
 }
+
